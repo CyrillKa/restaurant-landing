d650b353575dbda0911a01a654965fe8aad23bae
CyrillKa <kirill.klipka@yandex.ru>
2025-05-24 09:19:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Изменение цвета кнопки при клике
+const contactButton = document.querySelector(".contact-button");
+
+contactButton.addEventListener("click", () => {
+  alert("Спасибо за ваше обращение! Мы скоро свяжемся с вами.");
+});
